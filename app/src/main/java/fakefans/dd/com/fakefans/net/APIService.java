3c540f4f7c24afc3683d58e06523381e3faea557
adong <659579390@qq.com>
2016-08-23 11:10:53 +0800

    “列表数据增加缓存配置”

--- app/src/main/java/fakefans/dd/com/fakefans/net/APIService.java
+++ app/src/main/java/fakefans/dd/com/fakefans/net/APIService.java
@@ -27,6 +27,7 @@ public interface APIService {
     @GET("content/getcontentlist?categorytype=olympic&systype=cms&timestamp=1470901314&maxid=&sinceid=&adcode=110108&isoCC=cn&city=å\u008C\u0097äº¬å¸\u0082&device_product=Xiaomi&province=å\u008C\u0097äº¬å¸\u0082&userid=6497462&network_state=wifi&MNC=00&client_ver=5.3.2&client_code=73&udid=867389023462830&MCC=460&visit_id=1470901335384&device_os=6.0.1&longitude=116.336748&sp=ä¸\u00ADå\u009B½ç§»å\u008A¨&visit_start_time=1470901335384&ctime=1470901335381&sessionId=b8cb98182fd54c25a9d34c041229b7adwtmKUhdx&platform=android&app_key=2_2015_03_52&device_size=1080.0x1920.0&district=æµ·æ·\u0080å\u008Cº&latitude=40.029338&device_model=Xiaomi-MINOTELTE&securitykey=36540338832c4fb44572ae078c9ddc49&channel_num=xiaomi&citycode=010")
     Call<NewsGroupResult> getNewsGroups(@Query("categoryid") String id);
 
+    @Headers("Cache-Control: public, max-age=60")
     @GET("/255-1")//
     Observable<ShowApiResult<NewsData>> getNews(@Query("type") String type, @Query("title") String title, @Query("page") String page);
 }
